10faf0ea1037bf10c78c40839c0aab18a6f2d944
Sajeeb Karmaker Santo <73053229+sajeebKarmaker@users.noreply.github.com>
2025-03-13 01:01:16 +0600

    Basic structure done

---
+++ home-management/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
